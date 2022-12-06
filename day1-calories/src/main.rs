b2ecf62f9bcf8fbb6bd614b4316b58a796ec9654
Sara Brodin <sbrodin@Saras-MacBook-Pro.local>
2022-12-06 23:31:28 +0100

    refactored out file load

--- day1-calories/src/main.rs
+++ day1-calories/src/main.rs
@@ -1,52 +1,52 @@
 // TODO - REFACTOR
-// Break out file load
+// Break out file load - DONE
 // Find a better data structure
 
 
 use std::fs;
 use std::error::Error;
 
-// N*N
-pub fn max_calories_ineffective(input: &str) -> u32 {
-    let mut elfs = Vec::<u32>::new();
+mod calorie_counting {
+    pub fn load(input: &str, elfs: &mut Vec::<u32>) {
+        let mut itr = input.lines();
+        let mut sum = 0;
+        while let Some(line) = itr.next() {
+            if let true = line.is_empty() {
+                elfs.push(sum);
+                sum = 0;
+                continue;
+            };
 
-    // STORE
-    let mut itr = input.lines();
-    let mut sum = 0;
-    while let Some(line) = itr.next() {
-        if let true = line.is_empty() {
-            elfs.push(sum);
-            sum = 0;
-            continue;
+            sum += line.trim().parse::<u32>().unwrap();
         };
-
-        sum += line.trim().parse::<u32>().unwrap();
-        //println!("{}", line);
-    };
-    // Hack - push last elf
-    if sum > 0 {
-        elfs.push(sum);
-    }
-
-    // FIND
-    let mut max = 0;
-    let mut elf_index = 0;
-    for (i, elf) in elfs.iter().enumerate() {
-        if elf > &max {
-            max = *elf;
-            elf_index = i;
+        // Hack - push last elf
+        if sum > 0 {
+            elfs.push(sum);
         }
-        //println!("Calories: {}, Index: {}", elf, i);
     }
-    println!("Most calories: {}, By elf: {}", max, elf_index);
 
-    max
+    pub fn find_max_innefficent(elfs: &Vec::<u32>) -> u32 {
+        let mut max = 0;
+        let mut elf_index = 0;
+        for (i, elf) in elfs.iter().enumerate() {
+            if elf > &max {
+                max = *elf;
+                elf_index = i;
+            }
+            //println!("Calories: {}, Index: {}", elf, i);
+        }
+        println!("Most calories: {}, By elf: {}", max, elf_index);
+        max 
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = fs::read_to_string("input.txt")?;
+    let mut elfs = Vec::<u32>::new();
 
-    max_calories_ineffective(&file);
+    let file = fs::read_to_string("input.txt")?;
+    calorie_counting::load(&file, &mut elfs);
+    calorie_counting::find_max_innefficent(&elfs);
+
     Ok(())
 }
 
@@ -55,23 +55,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simple() {
+    fn simple_innefficent() {
+        let elfs = vec![1, 2, 4, 3];
+        assert_eq!(4, calorie_counting::find_max_innefficent(&elfs));
+    }
+
+    #[test]
+    fn load() {
         let input = "\
-1000
 2000
 3000
 
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
 10000";
+        let mut elfs = Vec::<u32>::new();
 
-        assert_eq!(24000, max_calories_ineffective(input));
+        calorie_counting::load(input, &mut elfs);
+        assert_eq!(
+            vec![5000, 10000],
+            elfs
+        );
     }
 }
