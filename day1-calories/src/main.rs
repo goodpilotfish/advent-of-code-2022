ff5717c5d5815c13f74fd28a338055a1a13ae41a
Sara Brodin <sbrodin@Saras-MacBook-Pro.local>
2022-12-06 23:14:10 +0100

    Solved day 1. Unoptimized

--- day1-calories/src/main.rs
+++ day1-calories/src/main.rs
@@ -1,17 +1,53 @@
-// Create modual
-// Create test
-// Impl modul
-// Impl reading from file
-// Done
+// TODO - REFACTOR
+// Break out file load
+// Find a better data structure
 
-mod elfs {
-    pub fn max_calories(_input: &str) -> u32 {
-        42
+
+use std::fs;
+use std::error::Error;
+
+// N*N
+pub fn max_calories_ineffective(input: &str) -> u32 {
+    let mut elfs = Vec::<u32>::new();
+
+    // STORE
+    let mut itr = input.lines();
+    let mut sum = 0;
+    while let Some(line) = itr.next() {
+        if let true = line.is_empty() {
+            elfs.push(sum);
+            sum = 0;
+            continue;
+        };
+
+        sum += line.trim().parse::<u32>().unwrap();
+        //println!("{}", line);
+    };
+    // Hack - push last elf
+    if sum > 0 {
+        elfs.push(sum);
     }
+
+    // FIND
+    let mut max = 0;
+    let mut elf_index = 0;
+    for (i, elf) in elfs.iter().enumerate() {
+        if elf > &max {
+            max = *elf;
+            elf_index = i;
+        }
+        //println!("Calories: {}, Index: {}", elf, i);
+    }
+    println!("Most calories: {}, By elf: {}", max, elf_index);
+
+    max
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = fs::read_to_string("input.txt")?;
+
+    max_calories_ineffective(&file);
+    Ok(())
 }
 
 #[cfg(test)]
@@ -36,6 +72,6 @@ mod tests {
 
 10000";
 
-        assert_eq!(24000, elfs::max_calories(input));
+        assert_eq!(24000, max_calories_ineffective(input));
     }
 }
