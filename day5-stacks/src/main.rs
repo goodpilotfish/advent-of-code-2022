7dad5540177f5b5aa25d7916c3acf3db0e52e83d
Sara Brodin <sbrodin@Saras-MacBook-Pro.local>
2022-12-09 16:19:21 +0100

    unit test work day 4

--- day5-stacks/src/main.rs
+++ day5-stacks/src/main.rs
@@ -3,15 +3,15 @@
 
 mod task {
     pub fn init_stacks(_input: &str, stack: &mut Vec<Vec<char>>) {
-        let v1 = vec!['Z', 'N', 'D'];
-        let v2 = vec!['M', 'C'];
+        let v1 = vec!['Z', 'N'];
+        let v2 = vec!['M', 'C', 'D'];
         let v3 = vec!['P'];
 
         *stack = vec![v1, v2, v3]
     }
 
     // move 1 from 2 to 1
-    pub fn operate_stacks(input: &str, stack: &Vec<&mut Vec<char>>) -> String {
+    pub fn operate_stacks(input: &str, stack: &mut Vec<Vec<char>>) -> String {
         input
             .lines()
             .for_each(|line| {
@@ -25,18 +25,37 @@ mod task {
                 match tmp[..] {
                     [crates, src, target] => {
                         println!("{}, {}, {}", crates, src, target);
-                        move_crates((crates, src, target), stack);
+                        *stack = move_crates((crates, src-1, target-1), stack);
                     },
                     _ => unreachable!(),
                 };
             }
         );
 
+        dbg!(stack);
+
         String::from("ABC")
     }
 
-    pub fn move_crates(input: (u32, u32, u32), _stack: &Vec<&mut Vec<char>>) {
-        input.0;
+    // move 1 from 2 to 1
+    pub fn move_crates(input: (u32, u32, u32), stack: &mut Vec<Vec<char>>) -> Vec<Vec<char>> {
+        let (nbr_crates, src, target) = input;
+        let mut tmp = Vec::<char>::new();
+        for _ in 0..nbr_crates { // POP
+            if let Some(top) = stack[src as usize].pop() {
+                println!("Pop: {top}");
+                tmp.push(top);
+            }
+        }
+
+        for _ in 0..nbr_crates { // PUSH
+            if let Some(top) = tmp.pop() {
+                println!("Push: {top}");
+                stack[target as usize].push(top);
+            }
+        }
+
+        stack.to_vec()
     }
 }
 
@@ -58,8 +77,8 @@ mod tests {
 1   2   3 ";
         let mut stacks: Vec<Vec<char>> = Vec::new();
 
-        let v1 = vec!['Z', 'N', 'D'];
-        let v2 = vec!['M', 'C'];
+        let v1 = vec!['Z', 'N'];
+        let v2 = vec!['M', 'C', 'D'];
         let v3 = vec!['P'];
         let res = vec![v1, v2, v3];
 
@@ -69,26 +88,31 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn operate_stacks() {
         let input = "\
 move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2"; 
-        let stacks: Vec<&mut Vec<char>> = Vec::new();
+        let mut stacks: Vec<Vec<char>> = Vec::new();
 
-        assert_eq!(String::from("CMZ"), task::operate_stacks(&input, &stacks));
+        assert_eq!(String::from("CMZ"), task::operate_stacks(&input, &mut stacks));
     }
 
     #[test]
     fn move_crates() {
-        let mut v1 = vec!['Z', 'N', 'D'];
-        let mut v2 = vec!['M', 'C'];
-        let mut v3 = vec!['P'];
-        let res = vec![&mut v1, &mut v2, &mut v3];
+        let v1 = vec!['Z', 'N'];
+        let v2 = vec!['M', 'C', 'D'];
+        let v3 = vec!['P'];
+        let mut input = vec![v1, v2, v3];
 
-        task::move_crates((1, 2, 3), &res);
+        let res: Vec<Vec<char>> = task::move_crates((1, 1, 0), &mut input);
 
-        //assert_eq()
+        let v1 = vec!['Z', 'N', 'D'];
+        let v2 = vec!['M', 'C'];
+        let v3 = vec!['P'];
+        let mut output = vec![v1, v2, v3];
+        assert_eq!(output, res);
     }
 }
